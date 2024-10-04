3bc377c37bd021acbe3b1ce361abcc9e7d398f5b
Tom Scanlan <tompscanlan@gmail.com>
2024-10-04 10:42:33 -0400

    fix: renaming and fixing ci

--- src/sub-categories/dto/update-subcategory.dto.ts
+++ src/sub-categories/dto/update-subcategory.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateSubCategoryDto } from './create-subCategory.dto';
+import { CreateSubCategoryDto } from './create-subcategory.dto';
 
 export class UpdateSubCategoryDto extends PartialType(CreateSubCategoryDto) {}
