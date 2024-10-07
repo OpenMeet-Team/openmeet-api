6c29d1a1da0a05995e5c67914d87ed343b1750d3
Tanzeel <tanzeelsaleem10@gmail.com>
2024-10-08 03:52:39 +0500

    fixes

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,12 +1,10 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TenantConnectionService } from '../tenant/tenant.service';
 import { GroupEntity } from './infrastructure/persistence/relational/entities/group.entity';
-import { CategoryEntity } from '../categories/infrastructure/persistence/relational/entities/categories.entity';
 import { GroupController } from './groups.controller';
 import { GroupService } from './groups.service';
 import { CategoryService } from '../categories/categories.service';
-import { EventsModule } from '../events/events.module';
 import { GroupMemberEntity } from '../group-members/infrastructure/persistence/relational/entities/group-member.entity';
 import { GroupUserPermissionEntity } from './infrastructure/persistence/relational/entities/group-user-permission.entity';
 import { UsersModule } from '../users/users.module';
@@ -15,7 +13,6 @@ import { UsersModule } from '../users/users.module';
   imports: [
     TypeOrmModule.forFeature([
       GroupEntity,
-      CategoryEntity,
       GroupMemberEntity,
       GroupUserPermissionEntity,
     ]),
