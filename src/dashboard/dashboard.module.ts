d42f9bd13a9854881a04b19d0b0c2395110ead58
Tom Scanlan <tompscanlan@gmail.com>
2024-10-11 13:36:24 -0400

    fix: fixup commit history

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
-import { EventsModule } from '../events/events.module';
+import { EventsModule } from '../event/event.module';
 import { TenantModule } from '../tenant/tenant.module';
-import { GroupModule } from '../groups/groups.module';
-import { CategoryModule } from '../categories/categories.module';
+import { GroupModule } from '../group/group.module';
+import { CategoryModule } from '../category/category.module';
 @Module({
   imports: [EventsModule, GroupModule, TenantModule, CategoryModule],
   controllers: [DashboardController],
