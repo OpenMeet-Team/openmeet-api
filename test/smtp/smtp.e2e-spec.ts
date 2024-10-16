af99f8827563c1050db7e5d344cd2e5b61fc84c6
Tom Scanlan <tompscanlan@gmail.com>
2024-10-16 11:12:51 -0400

    add smtp test

---
+++ test/smtp/smtp.e2e-spec.ts
@@ -0,0 +1,73 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+describe('SMTP (e2e)', () => {
+  let app: INestApplication;
+  let configService: ConfigService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    configService = moduleFixture.get<ConfigService>(ConfigService);
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('should connect to SMTP server and authenticate', async () => {
+    const smtpConfig = {
+      host: configService.get<string>('MAIL_HOST', { infer: true }),
+      port: configService.get<number>('MAIL_PORT', { infer: true }),
+      auth: {
+        user: configService.get<string>('MAIL_USER', { infer: true }),
+        pass: configService.get<string>('MAIL_PASSWORD', { infer: true }),
+      },
+      tls: {
+        rejectUnauthorized: false,
+        secure: configService.get<boolean>('MAIL_SECURE', { infer: true }),
+      },
+    };
+
+    const transporter = nodemailer.createTransport(smtpConfig);
+
+    await expect(transporter.verify()).resolves.toBe(true);
+  });
+
+  // skipping so we don't send mail frequently
+  it.skip('should send an email', async () => {
+    const smtpConfig = {
+      host: configService.get<string>('MAIL_HOST', { infer: true }),
+      port: configService.get<number>('MAIL_PORT', { infer: true }),
+      auth: {
+        user: configService.get<string>('MAIL_USER', { infer: true }),
+        pass: configService.get<string>('MAIL_PASSWORD', { infer: true }),
+      },
+      tls: {
+        rejectUnauthorized: false,
+        secure: configService.get<boolean>('MAIL_SECURE', { infer: true }),
+      },
+    };
+    const mailOptions = {
+      from: configService.get<string>('MAIL_DEFAULT_EMAIL', { infer: true }),
+      to: 'tom@openmeet.net',
+      subject: 'Test Email',
+      text: 'This is a test email',
+    };
+    console.log(mailOptions);
+    const transporter = nodemailer.createTransport(smtpConfig);
+
+    await expect(transporter.sendMail(mailOptions)).resolves.toEqual(
+      expect.objectContaining({
+        accepted: expect.arrayContaining([expect.any(String)]),
+      }),
+    );
+  });
+});
