11e9660973b228d8b34f13bee7a207b760d4f58d
codextech <tanzeelsaleem10@gmail.com>
2024-10-22 00:45:22 +0500

    event and group changes

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -71,8 +71,12 @@ export class GroupController {
 
   @Get('me/:id')
   @ApiOperation({ summary: 'Get group by ID Authenticated' })
-  async findOne(@Param('id') id: number): Promise<GroupEntity> {
-    const group = await this.groupService.findOne(+id);
+  async findOne(
+    @Param('id') id: number,
+    @AuthUser() user: User,
+  ): Promise<GroupEntity> {
+    const userId = user.id;
+    const group = await this.groupService.findQuery(+id, userId);
     if (!group) {
       throw new NotFoundException(`Group with ID ${id} not found`);
     }
