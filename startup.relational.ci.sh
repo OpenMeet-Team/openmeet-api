3337204e60ffaee702d25444f0739484dfaca805
Stan Khatko <stanislavkhatko@gmail.com>
2024-10-31 21:41:48 -0500

    fix: test

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -2,9 +2,8 @@
 set -e
 
 /opt/wait-for-it.sh postgres:5432
-# npm run migration:run:prod
-# npm run seed:run:prod
-npm run migration:reset
+npm run migration:run:prod
+npm run seed:run:prod
 
 npm run start:prod > prod.log 2>&1 &
 /opt/wait-for-it.sh maildev:1080
