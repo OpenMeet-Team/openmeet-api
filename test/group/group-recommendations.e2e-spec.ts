0dd0b55f9a0292aa2bfe1d29163b8d81cd5c5553
Stan Khatko <stanislavkhatko@gmail.com>
2024-11-07 13:35:58 -0500

    fix: fix

--- test/group/group-recommendations.e2e-spec.ts
+++ test/group/group-recommendations.e2e-spec.ts
@@ -162,7 +162,7 @@ describe('GroupRecommendations (e2e)', () => {
     //  group should exist
     const groupResponse = await request(APP_URL)
       .get(`/api/groups/${testGroup.id}`)
-      .set('Authorization', `Bearer ${token}`
+      .set('Authorization', `Bearer ${token}`)
       .set('x-tenant-id', TESTING_TENANT_ID);
 
     expect(groupResponse.status).toBe(200);
