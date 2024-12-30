c0c4bc97e37bca8fa4dda4f102337c2471095c2e
Tom Scanlan <tompscanlan@gmail.com>
2024-12-30 13:37:15 -0500

    save

---
+++ src/logger/simple.logger.ts
@@ -0,0 +1,21 @@
+import { LoggerService } from "@nestjs/common";
+
+export class SimpleLogger implements LoggerService {
+    log(message: string) {
+      console.log(message);
+    }
+    error(message: string, trace?: string) {
+      console.error(message);
+      if (trace) console.error(trace);
+    }
+    warn(message: string) {
+      console.warn(message);
+    }
+    debug(message: string) {
+      console.debug(message);
+    }
+    verbose(message: string) {
+      console.log(message);
+    }
+  }
+  
