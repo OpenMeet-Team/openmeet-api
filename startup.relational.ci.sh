7ba5aeff29fb449f29f605226e750ad28697e450
Tom Scanlan <tompscanlan@gmail.com>
2025-03-18 10:34:49 -0400

    don't keep container alive, let it exit

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -47,6 +47,6 @@ echo "Running E2E tests..."
 npm run test:e2e -- --runInBand
 
 # Keep the container running for debugging
-echo "Tests completed, keeping container alive for debugging..."
-sleep 90000
+# echo "Tests completed, keeping container alive for debugging..."
+# sleep 90000
 
