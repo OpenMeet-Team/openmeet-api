a404455485fee5af84c416cd78716a887c75d20f
Tom Scanlan <tom@openmeet.net>
2025-10-04 13:53:11 -0400

    fix: update JwtPayloadType to use secureId instead of numeric id

--- src/auth/strategies/types/jwt-payload.type.ts
+++ src/auth/strategies/types/jwt-payload.type.ts
@@ -2,7 +2,7 @@ import { Session } from '../../../session/domain/session';
 import { User } from '../../../user/domain/user';
 
 export type JwtPayloadType = Pick<User, 'id' | 'role' | 'slug'> & {
-  sessionId: Session['id'];
+  sessionId: Session['secureId'];
   tenantId: string;
   iat: number;
   exp: number;
